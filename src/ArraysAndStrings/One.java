347c29f626154345904608511f45533495a4f833
makerGeek <moetez.93@gmail.com>
2016-04-21 08:47:46 +0100

    1.1

--- src/ArraysAndStrings/One.java
+++ src/ArraysAndStrings/One.java
@@ -1,5 +1,22 @@
 package ArraysAndStrings;
 
+import java.util.*;
+
 public class One {
+    public static void main(String[] args) {
+        String s ="azertyui";
+        String q ="fmlsdqkjdfsd";
+        System.out.println(unique(s));
+        System.out.println(unique(q));
+    }
     
+    static boolean unique(String s){
+        Set<Character> set = new HashSet<>();
+        for(int i=0;i<s.length();i++){
+            if(set.contains(s.charAt(i))) return false;
+            set.add(s.charAt(i));
+            
+        }
+        return true;
+    }
 }
