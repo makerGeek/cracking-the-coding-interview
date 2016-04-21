5b062d2457262d3c4e3abbf6d7040ff25c2b639a
makerGeek <moetez.93@gmail.com>
2016-04-21 10:08:21 +0100

    Corrected main of Four.java to make it perform work on args[]

--- src/ArraysAndStrings/Four.java
+++ src/ArraysAndStrings/Four.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Four {
     public static void main(String[] args) {
-        String s ="hello good world!";
+        String s =args[0];
         System.out.println(Arrays.toString(replaceSpaces(s)));
         
         
