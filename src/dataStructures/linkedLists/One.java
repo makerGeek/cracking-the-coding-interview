98e7b038410a141b1edc7a5c867a8214ba32f672
makerGeek <moetez.93@gmail.com>
2016-04-21 13:13:11 +0100

    2.1

---
+++ src/dataStructures/linkedLists/One.java
@@ -0,0 +1,40 @@
+package dataStructures.linkedLists;
+import java.util.*;
+/**
+ *
+ * @author makerGeek
+ */
+public class One {
+    public static void main(String[] args) {
+        Node x = new Node(0);   
+        x.appendTail(new Node(5));
+        x.appendTail(new Node(3));
+        x.appendTail(new Node(7));
+        x.appendTail(new Node(8));
+        x.appendTail(new Node(5));
+        x.appendTail(new Node(4));
+        x.appendTail(new Node(2));
+        x.appendTail(new Node(8));
+        x.appendTail(new Node(2));
+        x.appendTail(new Node(5));
+        x.print();
+        
+        deleteDups(x);
+        x.print();
+    }
+    
+    
+    public static void deleteDups(Node head){
+        System.out.println("Deleting dups...");
+        Set<Integer> set = new HashSet<>();
+        set.add(head.val);
+        while(head.next!=null){
+            if(set.contains(head.next.val)) head.next=head.next.next;
+            else {
+                set.add(head.next.val);
+                head=head.next;
+            }
+        }
+        System.out.println("Dups deleted!");
+    }
+}
