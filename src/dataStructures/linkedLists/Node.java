a7c7ecd693d1e502adc9fd9788bc73a09c90e8da
makerGeek <moetez.93@gmail.com>
2016-04-21 12:51:45 +0100

    created class Node

---
+++ src/dataStructures/linkedLists/Node.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dataStructures.linkedLists;
+
+/**
+ *
+ * @author makerGeek
+ */
+public class Node {
+
+}
