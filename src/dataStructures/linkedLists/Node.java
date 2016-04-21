b9863d7d59859e6ad8ea4dc6646481e988ada5b1
makerGeek <moetez.93@gmail.com>
2016-04-21 12:54:04 +0100

    created class Node

--- src/dataStructures/linkedLists/Node.java
+++ src/dataStructures/linkedLists/Node.java
@@ -11,5 +11,13 @@ package dataStructures.linkedLists;
  * @author makerGeek
  */
 public class Node {
+    int val;
+    Node next;
+
+    public Node(int val) {
+        this.val = val;
+    }
+    
+    
 
 }
